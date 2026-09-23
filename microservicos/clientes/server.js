94b966e43a03c121db8adcd90fd08604c56ebc98
maurolibanore <maurolibanore10@gmail.com>
2026-09-23 08:37:17 -0300

    [Driver: Mauro] [Navigator: Individual] cria tabela clientes

--- microservicos/clientes/server.js
+++ microservicos/clientes/server.js
@@ -5,6 +5,18 @@ const app = express();
 
 app.use(express.json());
 
+async function criarTabela(){
+    await db.query(`
+        CREATE TABLE IF NOT EXISTS clientes (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        sobrenome VARCHAR(100) NOT NULL,
+        telefone VARCHAR(20),
+        email VARCHAR(150) NOT NULL UNIQUE
+        )
+        `);
+}
+
 app.listen(3003, () => {
     console.log("Clientes rodando na porta 3003");
 });
