f4b22baaceec2b2e59e7b74720dbbf17cbc933b3
maurolibanore <maurolibanore10@gmail.com>
2026-09-23 08:44:11 -0300

    [Driver: Mauro] [Navigator: Individual] implementa GET /clientes por id

--- microservicos/clientes/server.js
+++ microservicos/clientes/server.js
@@ -19,6 +19,29 @@ app.get("/clientes", async (req, res) => {
     }
 });
 
+app.get("/clientes/:id", async (req, res) => {
+    try {
+        const resultado = await db.query(
+            "SELECT * FROM clientes WHERE id = $1",
+            [req.params.id]
+        );
+
+        const cliente = resultado.rows[0];
+
+        if (!cliente) {
+            return res.status(404).json({
+                erro: "Cliente não encontrado"
+            });
+        }
+
+        res.json(cliente);
+    } catch (erro) {
+        res.status(500).json({
+            erro: "Erro ao buscar cliente"
+        });
+    }
+});
+
 async function criarTabela(){
     await db.query(`
         CREATE TABLE IF NOT EXISTS clientes (
