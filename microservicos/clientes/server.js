77d9a66ebbab43ea78054bcf00494c69d5d3cc4b
maurolibanore <maurolibanore10@gmail.com>
2026-09-23 08:42:20 -0300

    [Driver: Mauro] [Navigator: Individual] implementa GET /clientes

--- microservicos/clientes/server.js
+++ microservicos/clientes/server.js
@@ -5,6 +5,20 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/clientes", async (req, res) => {
+    try {
+        const resultado = await db.query(
+            "SELECT * FROM clientes ORDER BY id"
+        );
+
+        res.json(resultado.rows);
+    } catch (erro) {
+        res.status(500).json({
+            erro: "Erro ao buscar clientes"
+        });
+    }
+});
+
 async function criarTabela(){
     await db.query(`
         CREATE TABLE IF NOT EXISTS clientes (
@@ -15,7 +29,11 @@ async function criarTabela(){
         email VARCHAR(150) NOT NULL UNIQUE
         )
         `);
+
+        console.log("Tabela de clientes pronta");
 }
+
+criarTabela();
 
 app.listen(3003, () => {
     console.log("Clientes rodando na porta 3003");
